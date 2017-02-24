60d91cecbdc0db7e785eaf243ac8c37edb85de0e
tomtom80 <tom.klingbeil@gmail.com>
2017-02-24 16:02:20 +0100

    lecture 16

--- js_modules/webpack.config.js
+++ js_modules/webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
 
 const config = {
-    entry: './src/index.js', 
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                use:'babel-loader',
+                test: /\.js$/
+            }
+        ]
     }
 };
 
